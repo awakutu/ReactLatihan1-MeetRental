e25d20d432a35135773932f3db9194931cb0e8db
Rizky Tri Sulistyo <rizkytrisulistyo@gmail.com>
2020-11-10 17:43:46 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import CariRuang from './pages/CariRuangMeeting'
 import Login from './pages/login'
+import {default as Konfirmasi} from './pages/KonfirmasiRuangMeeting'
 
 export default function App() {
   return (
-  <Login/>
+  <Konfirmasi/>
   );
 }
 
