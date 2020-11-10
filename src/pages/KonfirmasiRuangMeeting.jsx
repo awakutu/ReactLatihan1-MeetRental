b4886f5a48f7c4316447a7f765f22b80f31d42e9
Rizky Tri Sulistyo <rizkytrisulistyo@gmail.com>
2020-11-10 17:43:37 +0700

    konfirmasi page

---
+++ src/pages/KonfirmasiRuangMeeting.jsx
@@ -0,0 +1,23 @@
+import "../component/meet.css";
+//import Meeting from "../component/meeting.svg";
+
+export default function KonfirmasiRuangMeeting() {
+  return (
+    <>
+      <div className="imgMeet"></div>
+      <div className="container">
+      <div className="row">
+          <div className="col-6 pt-5">
+              <h1>Meet Place Coworking Space</h1>
+              <p>Selasa, 3 November 2020</p>
+              <p>Kapasitas, 20 Orang</p>
+          </div>
+          <div className="col-6 pl-4 pt-5">
+              <h1>Lokasi</h1>
+              <p>Jalan Timun Laut, Block B. Jakarta Barat</p>
+          </div>
+      </div>
+      </div>
+     </>
+  );
+}
