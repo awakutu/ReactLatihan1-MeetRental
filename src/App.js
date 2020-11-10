e1dbf194934ea2a611bbd68cae9a5a56f5cf86b6
Rizky Tri Sulistyo <rizkytrisulistyo@gmail.com>
2020-11-10 18:57:04 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import CariRuang from './pages/CariRuangMeeting'
-import Login from './pages/login'
+import Login from './pages/Login'
 import {default as Konfirmasi} from './pages/KonfirmasiRuangMeeting'
 import Pembayaran from './pages/Pembayaran'
+import {default as Bukti} from './pages/BuktiPembayaran'
 
 export default function App() {
   return (
-  <Pembayaran/>
+  <Bukti/>
   );
 }
 
